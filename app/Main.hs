3fedf73fc3fed7f3586c525f276df03c2415f3af
Nate Nystrom <nate.nystrom@gmail.com>
2019-04-09 19:14:39 +0200

    moved tests to test directory

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,4 @@
 module Main where
 
-import qualified SourceList
-import qualified SourceMaybe
-import qualified SourceMonad
-import qualified SourcePrelude
-import qualified SourceTest
-import qualified SourceThih
-
 main :: IO ()
-main = do
-  -- SourceList.main
-  -- SourceMaybe.main
-  -- SourceMonad.main
-  -- SourcePrelude.main
-  SourceTest.main
-  -- SourceThih.main
+main = putStrLn "not implemented"
